66c0db890365b56939b1dce66e4b76d66aa0e25a
konrad.hojda <konrad.hojda@codete.com>
2019-02-25 12:20:23 +0100

    little changes about structure, added components and first fetching data from server

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, {Component} from 'react';
+import Titles from './components/Titles'
+import Form from './components/Form'
+import Weather from "./components/Weather";
+import axios from "axios";
+
+const API_KEY = '6e54d4d49a16d56ef4756ec5a1439e7d';
 
 class App extends Component {
+
+    getWeather = (e) => {
+        e.preventDefault();
+        const city = e.target.elements.city.value;
+        const country = e.target.elements.country.value;
+        axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`)
+            .then(response => console.log(response.data) );
+    };
+
     render() {
+
         return (
-           <div>
-                Hello!
-           </div>
+            <>
+                <Titles/>
+                <Form getWeather={this.getWeather}/>
+                <Weather/>
+            </>
         );
     }
 }
---
+++ src/components/Weather.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Weather extends Component {
+    render() {
+        return (
+            <p>Weather component works!</p>
+        );
+    }
+}
+
+export default Weather;
